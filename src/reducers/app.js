70db3999097bdd1db3d62d57f3e9aa2c4cbd5043
Lucas Vogel <contact@vogelino.ch>
2016-05-25 18:08:26 +0200

    Simplified the reducers combination

--- src/reducers/app.js
+++ src/reducers/app.js
@@ -1,18 +1,8 @@
 import counters from './counters'
 import * as actionTypes from '../actions/actionTypes';
 
-const initialState = {
-	counters: []
-};
-
-export default (state = initialState, action) => {
-	switch(action.type) {
-		case actionTypes.INCREMENT_COUNTER:
-		case actionTypes.DECREMENT_COUNTER:
-		case actionTypes.ADD_COUNTER:
-		case actionTypes.REMOVE_COUNTER:
-			return Object.assign({}, state,
-				{ counters: counters(state.counters, action) });
-		default: return state;
-	}
+export default (state = {}, action) => {
+	return {
+		counters: counters(state.counters, action)
+	};
 }
