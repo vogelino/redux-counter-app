3fa03d090c59c319cfb92049fe29f384b99ae0c3
Lucas Vogel <contact@vogelino.ch>
2016-05-25 14:08:30 +0200

    Added some test and babel presets to be in es6

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,5 @@
 
-module.exports = (state, action) => {
+export default (state = 0, action) => {
 	switch(action.type) {
 		case 'INCREMENT': return state + 1;
 		case 'DECREMENT': return state - 1;
--- test/reducers/TestCounterReducer.js
+++ test/reducers/TestCounterReducer.js
@@ -1,5 +1,8 @@
-const counter = require('../../src/reducers/counterReducer');
-const expect = require('expect');
+import counter from '../../src/reducers/counterReducer';
+import expect from 'expect';
+
+
+console.log('——————————————————— Test Counter Reducer ———————————————————');
 
 expect(
 	counter(0, { type: 'INCREMENT' })
@@ -12,6 +15,11 @@ expect(
 console.log('Test increment from 1 to 2 passed');
 
 expect(
+	counter(undefined, { type: 'INCREMENT' })
+).toEqual(1);
+console.log('Test from undefined to 1 passed');
+
+expect(
 	counter(2, { type: 'DECREMENT' })
 ).toEqual(1);
 console.log('Test decrement from 2 to 1 passed');
@@ -21,6 +29,15 @@ expect(
 ).toEqual(0);
 console.log('Test decrement from 1 to 0 passed');
 
-console.log('——————————————————— Test Counter Reducer ———————————————————');
-console.log('————————————————————— All tests passed —————————————————————');
+expect(
+	counter(1, { type: 'UNKNOWN_ACTION' })
+).toEqual(1);
+console.log('Test passing an unknown action retrieves the same state passed');
+
+expect(
+	counter(0, {})
+).toEqual(0);
+console.log('Test passing an invalid action retrieves the same state passed');
+
+console.log('————————————————————— All tests passed —————————————————————\n\n');
 
