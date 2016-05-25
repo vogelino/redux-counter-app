2a08fae75e60d7581e1264de8c6eb7cbb9455964
Lucas Vogel <contact@vogelino.ch>
2016-05-25 16:20:04 +0200

    Added tests for the ADD and REMOVE_COUNTER actions

--- test/reducers/TestCounterReducer.js
+++ test/reducers/TestCounterReducer.js
@@ -4,47 +4,97 @@ import expect from 'expect';
 
 
 test('TestCounterReducer', {
-	'Tests for the INCREMENT and DECREMENT actions': {
-		'Test increment from 0 to 1 passed': () => {
+	'INCREMENT and DECREMENT actions': {
+		'Increment from 0 to 1 passed': () => {
 			expect(
 				counter([0], { type: 'INCREMENT', index: 0 })
 			).toEqual([1]);
 		},
 
-		'Test increment from 1 to 2 passed': () => {
+		'Increment from 1 to 2 passed': () => {
 			expect(
 				counter([0,1], { type: 'INCREMENT', index: 1 })
 			).toEqual([0,2]);
 		},
 
-		'Test from undefined to empty array passed': () => {
+		'From undefined to empty array passed': () => {
 			expect(
 				counter(undefined, { type: 'INCREMENT', index: 0 })
 			).toEqual([]);
 		},
 
-		'Test decrement from 2 to 1 passed': () => {
+		'Decrement from 2 to 1 passed': () => {
 			expect(
 				counter([2], { type: 'DECREMENT', index: 0 })
 			).toEqual([1]);
 		},
 
-		'Test decrement from 1 to 0 passed': () => {
+		'Decrement from 1 to 0 passed': () => {
 			expect(
 				counter([1], { type: 'DECREMENT', index: 0 })
 			).toEqual([0]);
 		},
 
-		'Test passing an unknown action retrieves the same state passed': () => {
+		'Passing an unknown action retrieves the same state passed': () => {
 			expect(
 				counter([1], { type: 'UNKNOWN_ACTION' })
 			).toEqual([1]);
 		},
 
-		'Test passing an invalid action retrieves the same state passed': () => {
+		'Passing an invalid action retrieves the same state passed': () => {
 			expect(
 				counter([0], {})
 			).toEqual([0]);
+		}
+	},
+
+	'ADD_COUNTER and REMOVE_COUNTER actions': {
+		'From no counter to one counter': () => {
+			expect(
+				counter([], { type: 'ADD_COUNTER' })
+			).toEqual([0]);
+		},
+
+		'From one counter to two counter': () => {
+			expect(
+				counter([10], { type: 'ADD_COUNTER' })
+			).toEqual([10,0]);
+		},
+
+		'From one counter to no counter': () => {
+			expect(
+				counter([10], { type: 'REMOVE_COUNTER', index: 0 })
+			).toEqual([]);
+		},
+
+		'From three counter to two counter': () => {
+			expect(
+				counter([10, 11, 12], { type: 'REMOVE_COUNTER', index: 1 })
+			).toEqual([10, 12]);
+		},
+
+		'Add with undefined returns a new counter in array': () => {
+			expect(
+				counter(undefined, { type: 'ADD_COUNTER' })
+			).toEqual([0]);
+		},
+
+		'Remove with undefined returns an empty array': () => {
+			expect(
+				counter(undefined, { type: 'REMOVE_COUNTER', index: 0 })
+			).toEqual([]);
+		},
+
+		'Add with invalid action returns same state': () => {
+			expect(
+				counter([1,2,3], { type: 'REMOVE_COUNTER' })
+			).toEqual([1,2,3]);
+		},
+
+		'Remove with invalid action returns same state': () => {
+			expect(
+				counter([1,2,3], {})
+			).toEqual([1,2,3]);
 		}
 	}
 });
